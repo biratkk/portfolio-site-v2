8be2cd2fce3212f9579abd0d783581b4e1d360d4
Birat Koirala <koiralabirat@gmail.com>
2023-10-22 01:44:49 +0100

    Added changes to Amazon Experience

--- pages/components/pages/ExperiencePage.tsx
+++ pages/components/pages/ExperiencePage.tsx
@@ -9,9 +9,18 @@ const experienceDetails: ExperienceType[] = [
     fromDate: "3/7/2022",
     toDate: "Present",
     details: [
-      ``,
-      ``,
-      ``,
+      `Designed and built a proof of concept prototype UI design tool in
+React that improved developer workflow by 70% for a large-scale
+internal code generation engine. Integrated full-stack technologies to
+enable enhanced functionality.`,
+      `Bootstrapped and hosted scalable infrastructure on AWS services
+like Route 53, Lambda, API Gateway and S3 using aws-cdk.
+Established foundations for wider team adoption and growth.`,
+      `Implemented CI/CD pipeline to enable streamlined development
+through automated build, test and deployment processes.`,
+      `Led initial proof of concept phases and ensured smooth transition to
+internal team for full production development based on successful
+prototype.`,
     ],
     linkToCompany: "https://www.amazon.com",
   },
@@ -49,9 +58,9 @@ const ExperiencePage = ({}: ExperiencePageType) => {
   return (
     <div
       id="experience"
-      className="w-screen h-screen flex justify-center items-center dark:text-slate-300"
+      className="w-screen h-screen mb-20 flex justify-center items-center dark:text-slate-300"
     >
-      <div className="w-5/6 h-2/3 flex flex-col">
+      <div className="w-5/6 flex flex-col">
         <div className="text-center w-full">
           <HoverHeader>03. Experience</HoverHeader>
         </div>
