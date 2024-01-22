4cdb2ee36aed252a01c275d98dabae5f31e4f151
Birat Koirala <koiralabirat@gmail.com>
2024-01-22 13:10:57 +0000

    Added blog route to site

---
+++ pages/blog/[blogId].tsx
@@ -0,0 +1,66 @@
+import { useRouter } from "next/router";
+import {
+  Blog,
+  BlogEntryResponse,
+  getBlogEntry,
+} from "../components/contentful";
+import { NextPageContext } from "next";
+import { GetServerSidePropsContext } from "next";
+import ReactMarkdown from "react-markdown";
+import { Asset } from "contentful";
+import remarkGfm from "remark-gfm";
+import rehypekatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+import Link from "../components/atoms/Link";
+import { AnimatePresence } from "framer-motion";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { blogId } = context.query;
+  if (!blogId) {
+    throw new Error("Please make sure you have a valid blog (id).");
+  }
+  const blog = await getBlogEntry(blogId as string);
+  return { props: { blog } };
+}
+
+export default function ({ blog }: { blog: BlogEntryResponse }) {
+  console.log(blog.fields.authorImage);
+  return (
+    <div className="container dark:text-white px-4 md:px-0 h-fit mx-auto py-32">
+      <h1 className="text-5xl my-4 md:my-2 text-center font-bold">
+        {blog.fields.title}
+      </h1>
+      <div className="flex items-center justify-center gap-2 my-4">
+        <img
+          src={(blog.fields.authorImage as Asset).fields.file?.url?.toString()}
+          className="w-8 aspect-square rounded-full "
+        />
+        <p>{blog.fields.author}</p>
+        <p className="opacity-80">
+          -{" "}
+          {new Date(blog.fields.dateAndTime).toLocaleDateString("en-GB", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+      </div>
+      <div className="markdown">
+        <AnimatePresence>
+          <ReactMarkdown
+            components={{
+              a: (props) => {
+                return <Link href={props.href || "#"}>{props.children}</Link>;
+              },
+            }}
+            remarkPlugins={[remarkGfm, remarkMath]}
+            rehypePlugins={[rehypekatex]}
+          >
+            {blog.fields.blogContent}
+          </ReactMarkdown>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+}
---
+++ pages/blog/index.tsx
@@ -0,0 +1,64 @@
+import { BlogEntriesResponse, getBlogEntries } from "../components/contentful";
+import { useDarkMode } from "../../context/DarkModeContext";
+import Navbar from "../components/navbar";
+import { opensans } from "../../fonts";
+import Link from "next/link";
+import remarkGFM from 'remark-gfm'
+
+export async function getServerSideProps() {
+  const entries = await getBlogEntries();
+  return { props: { entries } };
+}
+
+export default function BlogListPage({
+  entries,
+}: {
+  entries: BlogEntriesResponse;
+}) {
+  return (
+    <>
+      <div className="pt-4">
+        <div className="container mx-auto px-4">
+          <div className="py-20">
+            <h1
+              style={opensans.style}
+              className="py-2 text-5xl font-open-sans my-4 text-green-500 dark:text-white rounded-r-md italic"
+            >
+              My Blogs
+            </h1>
+            <div className="grid grid-cols-1 gap-4">
+              {entries?.items.map((item) => (
+                <BlogPreview id={item.sys.id} {...item.fields} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function BlogPreview({
+  title,
+  dateAndTime,
+  previewDescription,
+  id,
+}: BlogEntriesResponse["items"][number]["fields"] & { id: string }) {
+  return (
+    <Link href={`/blog/${id}`}>
+      <div className="overflow-hidden h-44 bg-white dark:text-white dark:bg-slate-700 duration-300 dark:shadow-none dark:outline dark:outline-1 dark:hover:outline-green-200 dark:hover:shadow hover:cursor-pointer dark:outline-white shadow-md hover:shadow-lg dark:shadow-white rounded-lg">
+        <div className="p-4 dark:bg-green-500 dark:bg-opacity-75 bg-green-100">
+          <h2 className=" text-xl font-semibold dark:text-white text-slate-700">
+            {title}
+          </h2>
+        </div>
+        <span className="px-4 text-xs opacity-50">
+          {new Date(dateAndTime as string).toUTCString()}
+        </span>
+        <div className="px-4 py-1">
+          <p>{previewDescription}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
