96ac833b8c2b18d9dc7e9ec24865b7e86c058931
biratkk <koiralabirat@gmail.com>
2024-03-10 20:59:52 +0000

    Resolved conflicts

--- components/atoms/HighlightText.tsx
+++ components/atoms/HighlightText.tsx
@@ -5,11 +5,7 @@ type HighlightTextType = {
 }
 const HighlightText = ({children}:HighlightTextType) => {
   return (
-<<<<<<< Updated upstream
-    <HighlightWrapper>{children}</HighlightWrapper>
-=======
     <span className="text-green-600">{children}</span>
->>>>>>> Stashed changes
   )
 };
 
